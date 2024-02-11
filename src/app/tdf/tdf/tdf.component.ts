16ecb848691fde2ae6ed21dc4f94a77e4205f9d0
Naushad <naushad.a1013@gmail.com>
2024-02-12 03:41:30 +0530

    TDF implemented

--- src/app/tdf/tdf/tdf.component.ts
+++ src/app/tdf/tdf/tdf.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import {  AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-tdf',
   templateUrl: './tdf.component.html',
   styleUrl: './tdf.component.css'
 })
-export class TdfComponent {
+export class TdfComponent implements AfterViewInit{
+  submitted:boolean=false;
+  users:any[]=[]
+  @ViewChild('userform') formObject!:NgForm;
 
+  ngAfterViewInit(): void {
+    this.setDefault()
+  }
+
+  private setDefault(){
+    setTimeout(() => {
+      if(this.formObject.controls["userdata"]){
+       this.formObject.form.setValue({
+           gender:"Male",
+           userdata:{
+               username:'Naushad',
+               email:'cognizant@mail.com'
+             },
+           
+           })
+         }
+         
+         this.formObject.form.patchValue({
+           userdata:{
+             username:null,
+             email:'humana@mail.com'
+           },
+           gender:'Male',
+         })
+       
+   }, 1);
+  }
+
+  onSubmit(){
+    // console.log(this.formObject.value)
+
+    this.submitted=true
+    this.users.push({
+      id:Number(Math.floor(Math.random()*1000+1000).toString()+this.users.length.toString()),
+      name:this.formObject.value.userdata.username,
+      email:this.formObject.value.userdata.email,
+      gender:this.formObject.value.gender
+    })
+    
+    this.reset()
+
+  }
+
+  reset(){
+    this.formObject.form.reset()
+    this.setDefault()
+  }
+  clear(){
+    this.users=[]
+  }
 }
